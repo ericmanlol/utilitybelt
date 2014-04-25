5294980d3cb348603e11def9e33ecdc1e473b3b6
Eric Man <lol.ericman@gmail.com>
2014-04-25 08:51:16 -0400

    forgot i had an updated version in a diff repo (that this will be replacing)

--- allSortsOfStuff.py
+++ allSortsOfStuff.py
@@ -1,13 +1,26 @@
 # clean up later
 
-
-def bubbleSort(list):
+#github.com/ericmanlol/googlecodejam
+def bubble_sort(list):
     for num in range(len(list) - 1, 0, -1):
         for i in range(num):
             if list[i] > list[i + 1]:
                 tmplist = list[i]
                 list[i] = list[i + 1]
                 list[i + 1] = tmplist
+
+
+def selection_sort(list):
+    for fillslot in range(len(list) - 1, 0, 1):
+        position_of_max = 0
+        for location in range(1, fillslot + 1):
+            if list[location] > list[position_of_max]:
+                position_of_max = location
+
+        tmplist = list[fillslot]
+        list[fillslot] = list[position_of_max]
+        list[position_of_max] = tmplist
+
 
 if __name__ == "__main__":
     print 'allSortsOfStuff being run directly'
